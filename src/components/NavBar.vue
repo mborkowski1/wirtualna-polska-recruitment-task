5b71c8fd236764c519eef43320e2608480619e4f
Michał Borkowski <borkowski120@wp.pl>
2020-11-23 13:59:59 +0100

    Create project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar app color="primary" dark>
+
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AppBar'
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
