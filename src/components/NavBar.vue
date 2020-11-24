da898f6392896bfb6464f427725e4bb986e9c796
Michał Borkowski <borkowski120@wp.pl>
2020-11-24 08:47:45 +0100

    Add dropdown menu to app bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,26 @@
 <template>
   <v-app-bar app color="primary" dense dark>
-    <v-tabs centered>
+    <v-tabs centered class="hidden-sm-and-down">
       <v-tab v-for="menuItem in menuItems" :key="menuItem.title" :to="menuItem.path">
         {{menuItem.title}}
       </v-tab>
     </v-tabs>
+
+    <div class="hidden-md-and-up">
+      <v-menu class="hidden-sm-and-up">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="menuItem in menuItems" :key="menuItem.title" :to="menuItem.path">
+            <v-list-item-title>{{menuItem.title}}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
   </v-app-bar>
 </template>
 
